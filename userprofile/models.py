baac680f2e0e52c9bd9a83b2d80ac9b6796ad2b6
dark850916 <dark850916@gmail.com>
2024-02-11 09:32:55 +0100

    Adicionado el modelo a la base de datos de user de django al modelo de userprofile

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class UserProfile(models.Model):
     uid = models.CharField(max_length=36)
-    days_of_test = models.PositiveIntegerField( default=15)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    days_of_test = models.PositiveIntegerField(default=15)
     days_of_use_as_premiun = models.PositiveIntegerField(default=30)
     start_date_for_test = models.DateField(auto_now_add=True)
     start_date_for_premiun_use = models.DateField(auto_now_add=True)
