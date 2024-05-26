df37628f465e40a7a89bb5d9d65a47fe5953c9ac
navegabit <navegabit.2020@gmail.com>
2024-05-26 09:57:21 +0200

    Feature: Server Navegabit

--- tkinter-validator-frontend/src/apps/utils/decorators.py
+++
@@ -1,8 +0,0 @@
-# Decorators functions for manage common functionalities.
-def error_handler(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-        return wrapper
--- tkinter-validator-frontend/src/services/chatgpt/descargar.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-if 'icons' not in locals():
-  with open('/content/iconos_train.pkl', 'rb') as handle:
-      icons = pickle.load(handle)
